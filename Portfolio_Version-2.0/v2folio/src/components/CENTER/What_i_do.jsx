6b221bb39ebe1fae3c171dcbcff95d85c2ee7874
AkashDurai <93862553+akashdurai4274@users.noreply.github.com>
2024-07-20 22:03:14 +0530

    Update What_i_do.jsx

--- Portfolio_Version-2.0/v2folio/src/components/CENTER/What_i_do.jsx
+++ Portfolio_Version-2.0/v2folio/src/components/CENTER/What_i_do.jsx
@@ -2,17 +2,13 @@ import ActivityBox from "./ActivityBox"
 
 const What_i_do = () => {
   const uiux = "Creating intuitive and engaging user experiences through thoughtful design, wireframing, and prototyping to ensure seamless interaction and visual appeal."
-  const web = "Building responsive & dynamic websites using modern technologies and frameworks, ensuring functionality, performance, and cross-browser compatibility.";
-  const app = "Developing user-friendly mobile applications with a focus on security, performance,  and seamless user experience for both iOS and Android platforms."
-  const pbs = "Tackling complex challenges with analytical thinking, innovative solutions, and efficient algorithms to deliver optimized and effective results."
+   const pbs = "Tackling complex challenges with analytical thinking, innovative solutions, and efficient algorithms to deliver optimized and effective results."
   return (
-    <div className="mt-5">
+    <div >
       <span className="text-3xl font-bold font-rajdhani">What I Do</span>
       <div className="container flex flex-wrap justify-between">
         <ActivityBox id="1" topic="Web Development" data={web} color="green"/>
         <ActivityBox id="2" topic="App Development" data={app} color="red"/>
-        <ActivityBox id="3" topic="UI/Ux" data={uiux} color="yellow"/>
-        <ActivityBox id="4" topic="Problem Soving" data={pbs} color="violet"/>
       </div>
     </div>
   );
