8442fe1569ba1c029ccb6bab8135839b484e77db
AkashDurai <93862553+akashdurai4274@users.noreply.github.com>
2024-07-20 22:12:40 +0530

    Delete Employee_Tracker/Backend/routes/employeeRoutes.js

--- Employee_Tracker/Backend/routes/employeeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { addEmployee, getEmployees, deleteEmployee } = require('../controllers/employeeController');
-const authMiddleware = require('../middlewares/authMiddleware');
-const router = express.Router();
-
-router.route('/').post(addEmployee).get(getEmployees);
-router.route('/:id').delete(authMiddleware, deleteEmployee);
-
-module.exports = router;
